f9710db0040b30d955a37ab1208fb1372620698d
Igor Gajsin <igajsin@mirantis.com>
2016-07-06 13:46:53 +0300

    add the populate_hiera functiona and some jump around puppets

--- deployment_scripts/puppet/manifests/populate_hiera.pp
+++ deployment_scripts/puppet/manifests/populate_hiera.pp
@@ -1,3 +1,12 @@
-notify {'MODULAR: manila/override_hiera'}
+notify {'MODULAR: manila/populate_hiera': }
 
-populate_hiera('/etc/hiera/plugins/fuel-plugin-manila.yaml')
+$manila       = hiera_hash('manila', false)
+$file         = '/etc/hiera/plugins/fuel-plugin-manila.yaml'
+$new_password = generate("/bin/bash", "-c", "/bin/date +%s | sha256sum | base64 | head -c 32 ; echo -n")
+
+if ! $manila {
+  notify {"DBG! manila is empty. Do something ${new_npassword}": }
+  populate_hiera($file, 'user_password', $new_password)
+  } else {
+  notify {"DBG! manila non empty. is ${manila}": }
+  }
