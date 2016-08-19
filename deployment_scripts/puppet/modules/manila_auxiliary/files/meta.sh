54c85cb46d8f2b7dcc8f3b6942f011d1f6911be2
Igor Gajsin <igajsin@mirantis.com>
2016-08-19 16:37:36 +0300

    Fix meta.sh
    Change-Id: I7eae24bd6c5bb6856db1a98287b8fb805d37c8dc

--- deployment_scripts/puppet/modules/manila_auxiliary/files/meta.sh
+++ deployment_scripts/puppet/modules/manila_auxiliary/files/meta.sh
@@ -2,12 +2,12 @@
 
 . /root/openrc
 
-if  manila type-list | grep -q 'default_share_type'; then
+if ! manila type-list | grep -q 'default_share_type'; then
     echo 'add default_share_type'
     manila type-create default_share_type True
 fi
 
-if  manila share-network-list | grep -q 'test_share_network'; then
+if ! manila share-network-list | grep -q 'test_share_network'; then
     echo 'add test_share_network'
     net_uid=$(neutron net-list | grep -q 'internal' | cut -f2 -d' ')
     subnet_uid=$(neutron net-list | grep -q 'internal' | cut -f6 -d' ')
@@ -17,7 +17,7 @@ if  manila share-network-list | grep -q 'test_share_network'; then
 	   --neutron-subnet-id $subnet_uid
 fi
 
-if  openstack flavor list | grep -q 'manila-service-flavor'; then
+if ! openstack flavor list | grep -q 'manila-service-flavor'; then
     echo 'add manila-service-flavor'
     openstack flavor create manila-service-flavor \
       --id 100  \
