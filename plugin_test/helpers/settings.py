77075600b8b2bf8c9ec9a6a4bcc8d97101de92c1
Volodymyr Rozhanskyy <vrozhanskyy@mirantis.com>
2016-09-06 12:17:16 +0300

    refactored smoke+bvt
    Change-Id: Ifeda0a0f84fa64f8a893f67ca406f34df5c5813f

--- plugin_test/helpers/settings.py
+++ plugin_test/helpers/settings.py
@@ -13,9 +13,8 @@ License for the specific language governing permissions and limitations
 under the License.
 """
 
-
 import os
-MANILA_PLUGIN_VERSION = os.environ.get('MANILA_PLUGIN_VERSION')
+MANILA_PLUGIN_VERSION = os.environ.get('MANILA_PLUGIN_VERSION', '1.0.1')
 MANILA_PLUGIN_PATH = os.environ.get('MANILA_PLUGIN_PATH')
 MANILA_IMAGE_PATH = os.environ.get('MANILA_IMAGE_PATH')
 plugin_name = 'fuel-plugin-manila'
