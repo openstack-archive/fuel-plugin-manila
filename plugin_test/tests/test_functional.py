cfe241f38dc8e6c27c6d00ef917506bde2bec170
Volodymyr Rozhanskyy <vrozhanskyy@mirantis.com>
2016-09-12 15:39:48 +0300

    functional test manila_del_add_сompute added
    Change-Id: I8c831e5d5cc1c81c6bcb4018f2a4f3cd9cb8ba27

--- plugin_test/tests/test_functional.py
+++ plugin_test/tests/test_functional.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 """Copyright 2016 Mirantis, Inc.
 
 Licensed under the Apache License, Version 2.0 (the "License"); you may
@@ -23,7 +24,7 @@ from helpers import plugin
 from proboscis import test
 
 
-@test(groups=['manila_plugin', 'manila_integration'])
+@test(groups=['manila_integration'])
 class TestManilaFunctional(TestBasic):
     """Functional test suite.
 
@@ -128,6 +129,119 @@ class TestManilaFunctional(TestBasic):
         self.show_step(11)
         self.fuel_web.update_nodes(
             cluster_id, {'slave-01': ['controller']})
+
+        self.show_step(12)
+        self.fuel_web.deploy_cluster_wait(
+            cluster_id,
+            check_services=False
+        )
+        self.show_step(13)
+        self.fuel_web.run_ostf(
+            cluster_id=cluster_id,
+            should_fail=0,
+            test_sets=['smoke', 'sanity', 'ha'])
+
+        self.show_step(14)
+        TestPluginCheck(self).plugin_check()
+
+    @test(depends_on=[SetupEnvironment.prepare_slaves_5],
+          groups=["manila_del_add_сompute"])
+    @log_snapshot_after_test
+    def manila_del_add_сompute(self):
+        """Check deploy with Manila Plugin after controller remove and add.
+
+        Scenario:
+            1. Upload plugins and install.
+            2. Create environment :
+                * Networking: Neutron with tunneling segmentation
+                * Block Storage: Ceph
+                * Other Storages: all Ceph
+                * Additional services: disabled
+            3. Enable plugin and add nodes with following roles:
+                * Controller + Ceph-OSD
+                * Compute + Manila-data + Manila-share
+                * Compute
+                * Ceph-OSD
+                * Ceph-OSD
+            4. Deploy cluster with plugin.
+            5. Run OSTF
+            6. Verify Manila service basic functionality (share create/mount).
+            7. Delete node with compute role
+            8. Deploy changes
+            9. Run OSTF
+            10. Verify Manila service basic functionality (share create/mount).
+            11. Add a node with compute role
+            12. Deploy changes
+            13. Run OSTF
+            14. Verify Manila service basic functionality (share create/mount).
+
+        """
+
+        self.env.revert_snapshot("ready_with_5_slaves")
+        self.show_step(1)
+        plugin.install_manila_plugin(self.ssh_manager.admin_ip)
+        plugin.upload_manila_image(self.ssh_manager.admin_ip)
+
+        self.show_step(2)
+        cluster_id = self.fuel_web.create_cluster(
+            name=self.__class__.__name__,
+            mode=DEPLOYMENT_MODE,
+            settings={
+                "net_provider": 'neutron',
+                "net_segment_type": 'tun',
+                'volumes_lvm': False,
+                'volume_ceph': True,
+                "image_ceph": True,
+                "ephemeral_ceph": True,
+                "objects_ceph": True
+            }
+        )
+
+        self.show_step(3)
+        plugin.enable_plugin_manila(cluster_id, self.fuel_web)
+        self.fuel_web.update_nodes(
+            cluster_id,
+            {'slave-01': ['controller', 'ceph-osd'],
+             'slave-02': ['compute', 'manila-data', 'manila-share'],
+             'slave-03': ['compute'],
+             'slave-04': ['ceph-osd'],
+             'slave-05': ['ceph-osd']
+             }
+        )
+
+        self.show_step(4)
+        self.fuel_web.deploy_cluster_wait(cluster_id)
+
+        self.show_step(5)
+        self.fuel_web.run_ostf(cluster_id=cluster_id,
+                               test_sets=['smoke', 'sanity'])
+
+        self.show_step(6)
+        TestPluginCheck(self).verify_manila_functionality()
+
+        self.show_step(7)
+        self.fuel_web.update_nodes(
+            cluster_id, {'slave-03': ['compute']},
+            pending_addition=False, pending_deletion=True)
+
+        self.show_step(8)
+        self.fuel_web.deploy_cluster_wait(
+            cluster_id,
+            check_services=False
+        )
+
+        self.show_step(9)
+        self.fuel_web.run_ostf(
+            cluster_id=cluster_id,
+            should_fail=0,
+            test_sets=['smoke', 'sanity', 'ha'])
+
+        self.show_step(10)
+        TestPluginCheck(self).plugin_check()
+
+        self.show_step(11)
+        self.fuel_web.update_nodes(
+            cluster_id, {'slave-03': ['compute']})
 
         self.show_step(12)
         self.fuel_web.deploy_cluster_wait(
