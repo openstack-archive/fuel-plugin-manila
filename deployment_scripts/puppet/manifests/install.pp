c09287c523b5b59a445c2f54fd962d566bbbe82c
Igor Gajsin <igajsin@mirantis.com>
2016-09-28 18:03:29 +0300

    Fix installation
    There was the unexpected problem. Suddenly the python-heat package
    depends on the python-manilaclient package in ubuntu. And when the
    plugin removes python-maniliaclient for changes this by its own ones,
    this action destroys the heat completely.
    
    This patch  provides the workaround of that.
    
    Change-Id: I5f6d963a1783eba838afdb93eef19108b70d5417
    Closes-bug: #1628063

--- deployment_scripts/puppet/manifests/install.pp
+++ deployment_scripts/puppet/manifests/install.pp
@@ -1,5 +1,10 @@
 notify {'MODULAR: fuel-plugin-manila/install': }
 
+$master_ip             = hiera('master_ip')
+$manilaclient_pkg_name = 'fuel-plugin-manila-manilaclient'
+$manilaclient_pkg      = "${manilaclient_pkg_name}_1.8.2_all.deb"
+$manilaclient_pkg_url  = "http://${master_ip}:8080/plugins/fuel-plugin-manila-1.0/repositories/ubuntu/${manilaclient_pkg}"
+
 package {'python-pip':
   ensure => 'installed'
 }
@@ -16,9 +21,12 @@ package {'pycrypto':
   provider => 'pip',
 }
 
-package {'python-manilaclient':
-  ensure => 'absent'
+exec { 'install_manilaclient':
+  path    => '/usr/sbin:/usr/bin:/sbin:/bin:',
+  command => "wget ${manilaclient_pkg_url} -O /tmp/${manilaclient_pkg} && dpkg --force-overwrite -i /tmp/${manilaclient_pkg}",
+  onlyif  => "echo \"! dpkg -l ${manilaclient_pkg_name}\" | bash",
 }
+
 
 package {'python-manila':
   ensure => 'absent'
@@ -40,10 +48,6 @@ package {'fuel-plugin-manila-manila-core':
   ensure => 'installed'
 }
 
-package {'fuel-plugin-manila-manilaclient':
-  ensure => 'installed'
-}
-
 package {'fuel-plugin-manila-manila-ui':
   ensure => 'installed'
 }
@@ -54,11 +58,10 @@ Package['python-pip']->
 Package['python-dev']->
 Package['python-pymysql']->
 Package['pycrypto']->
-Package['python-manilaclient']->
 Package['python-manila']->
 Package['manila-api']->
 Package['manila-common']->
 Package['manila-scheduler']->
 Package['fuel-plugin-manila-manila-core']->
-Package['fuel-plugin-manila-manilaclient']->
+Exec['install_manilaclient']->
 Package['fuel-plugin-manila-manila-ui']
