4d00d08f8d3054379d0390d827cf3c18f7be70e8
Volodymyr Rozhanskyy <vrozhanskyy@mirantis.com>
2016-09-30 16:51:32 +0300

    added test manila_del_add_cinder
    Change-Id: I51778d016ac26bb17a63e83004a52dcf2d375fd8

--- plugin_test/tests/test_functional.py
+++ plugin_test/tests/test_functional.py
@@ -248,3 +248,113 @@ class TestManilaFunctional(TestBasic):
         self.show_step(14)
         TestPluginCheck(self).verify_manila_functionality()
 
+    @test(depends_on=[SetupEnvironment.prepare_slaves_5],
+          groups=["manila_del_add_cinder"])
+    @log_snapshot_after_test
+    def manila_del_add_cinder(self):
+        """Check deploy after cinder node remove and add.
+
+        Scenario:
+            1. Upload plugins and install.
+            2. Create environment :
+                * Networking: Neutron with VLAN segmentation
+                * Block Storage: LVM
+                * Other Storages: default
+                * Additional services: disabled
+            3. Enable plugin and add nodes with following roles:
+                * Controller
+                * Cinder
+                * Cinder
+                * Compute
+                * Base-OS + Manila-data + Manila-share
+            4. Deploy cluster with plugin.
+            5. Run OSTF
+            6. Verify Manila service basic functionality (share create/mount).
+            7. Delete node with cinder role
+            8. Deploy changes
+            9. Run OSTF
+            10. Verify Manila service basic functionality (share create/mount).
+            11. Add a node with cinder role
+            12. Deploy changes
+            13. Run OSTF
+            14. Verify Manila service basic functionality (share create/mount).
+
+        """
+
+        self.env.revert_snapshot("ready_with_5_slaves")
+        self.show_step(1)
+        plugin.install_manila_plugin(self.ssh_manager.admin_ip)
+        plugin.upload_manila_image(self.ssh_manager.admin_ip)
+
+        self.show_step(2)
+        cluster_id = self.fuel_web.create_cluster(
+            name=self.__class__.__name__,
+            settings={
+                "net_provider": 'neutron'
+            }
+        )
+
+        self.show_step(3)
+        plugin.enable_plugin_manila(cluster_id, self.fuel_web)
+        self.fuel_web.update_nodes(
+            cluster_id,
+            {'slave-01': ['controller'],
+             'slave-02': ['cinder'],
+             'slave-03': ['cinder'],
+             'slave-04': ['compute'],
+             'slave-05': ['base-os', 'manila-data', 'manila-share']
+             }
+        )
+
+        self.show_step(4)
+        self.fuel_web.deploy_cluster_wait(
+            cluster_id,
+            check_services=False
+        )
+
+        self.show_step(5)
+        self.fuel_web.run_ostf(cluster_id=cluster_id,
+                               test_sets=['smoke', 'sanity'])
+
+        self.show_step(6)
+        TestPluginCheck(self).verify_manila_functionality()
+
+        self.show_step(7)
+        self.fuel_web.update_nodes(
+            cluster_id, {'slave-03': ['cinder']},
+            pending_addition=False, pending_deletion=True)
+
+        self.show_step(8)
+        self.fuel_web.deploy_cluster_wait(
+            cluster_id,
+            check_services=False
+        )
+
+        self.show_step(9)
+        self.fuel_web.run_ostf(
+            cluster_id=cluster_id,
+            should_fail=0,
+            test_sets=['smoke', 'sanity'])
+
+        self.show_step(10)
+        TestPluginCheck(self).verify_manila_functionality()
+
+        self.show_step(11)
+        self.fuel_web.update_nodes(
+            cluster_id, {'slave-03': ['cinder']})
+
+        self.show_step(12)
+        self.fuel_web.deploy_cluster_wait(
+            cluster_id,
+            check_services=False
+        )
+        self.show_step(13)
+        self.fuel_web.run_ostf(
+            cluster_id=cluster_id,
+            should_fail=0,
+            test_sets=['smoke', 'sanity'])
+
+        self.show_step(14)
+        TestPluginCheck(self).verify_manila_functionality()
+
+
