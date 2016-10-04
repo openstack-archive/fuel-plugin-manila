f30b72a42bf32a2383a1f606000f97ec661a660c
Igor Gajsin <igajsin@mirantis.com>
2016-10-04 19:38:12 +0300

    Remote creation default share type and share network
    Previously default share type and share network were created by plugin.
    That was helpful for testing when development but it is odd behaviour
    for production.
    
    Change-Id: I4926cb8f2890b077dfd0471d38af75f67082796c

--- deployment_scripts/puppet/modules/manila_auxiliary/files/meta.sh
+++ deployment_scripts/puppet/modules/manila_auxiliary/files/meta.sh
@@ -2,21 +2,6 @@
 
 . /root/openrc
 
-if ! manila type-list | grep -q 'default_share_type'; then
-    echo 'add default_share_type'
-    manila type-create default_share_type True
-fi
-
-if ! manila share-network-list | grep -q 'test_share_network'; then
-    echo 'add test_share_network'
-    net_uid=$(neutron net-list | grep  'internal' | cut -f2 -d' ')
-    subnet_uid=$(neutron net-list | grep  'internal' | cut -f6 -d' ')
-    manila share-network-create \
-	   --name test_share_network  \
-	   --neutron-net-id $net_uid  \
-	   --neutron-subnet-id $subnet_uid
-fi
-
 if ! openstack flavor list | grep -q 'manila-service-flavor'; then
     echo 'add manila-service-flavor'
     openstack flavor create manila-service-flavor \
